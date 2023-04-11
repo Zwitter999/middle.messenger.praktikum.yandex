d95f944101948efce6c8290da2a8963e5207df3d
Sigurd_prog <krupotkin@list.ru>
2023-04-11 19:52:48 +0300

    Первые правки

--- src/controllers/ValidateFormsController.test.ts
+++ src/controllers/ValidateFormsController.test.ts
@@ -1,23 +1,20 @@
 import { expect } from 'chai';
 import ValidateFormsController from './ValidateFormsController';
-import SignupPage from '../ui/pages/SignupPage/SignupPage';
+import ErrorText from '../ui/components/Errortext/ErrorText';
 
 describe('ValidateFormsController.test', () => {
   it('validate name', () => {
-    const page = SignupPage;
+    const page = new ErrorText({ text: 'test' });
     const value = 'Jhon';
-    const type = 'name';
+    const type = 'first_name';
     const error = ValidateFormsController.onBlurFocus(page, value, type);
-
     expect(error).to.equal(false);
   });
-
   it('validate unsuitable password', () => {
-    const page = SignupPage;
+    const page = new ErrorText({ text: 'test' });
     const value = 'Jhon';
     const type = 'password';
     const error = ValidateFormsController.onBlurFocus(page, value, type);
-
     expect(error).to.equal(true);
   });
 });
